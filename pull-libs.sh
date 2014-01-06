39446a3a22b5d05c3476890a59c29d105b622f84
Florian Jakob <github@fjak.de>
2014-01-06 11:53:41 +0100

    Add shellscript to resolve dependencies

---
+++ pull-libs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+cd $(dirname "$BASH_SOURCE")
+mkdir -p lib
+cd lib
+
+curl --location --remote-name http://update.sugarj.org/plugins/org.spoofax.jsglr_1.2.0.201309171843.jar
+curl --location --remote-name http://update.sugarj.org/plugins/org.spoofax.terms_1.2.0.201309171843.jar
+
+mv org.spoofax.jsglr_1.2.0.201309171843.jar org.spoofax.jsglr.jar
+mv org.spoofax.terms_1.2.0.201309171843.jar org.spoofax.terms.jar
